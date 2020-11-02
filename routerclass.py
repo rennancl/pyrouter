43fc4a4af4965cc0d7cba72bca44a1c7413aa01e
Rennan Cordeiro <rennancl@gmail.com>
2020-11-02 16:11:15 -0300

    fixes tracing

--- routerclass.py
+++ routerclass.py
@@ -5,8 +5,8 @@ class Router:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
-        self.table = {}
-        self.routes = {}
+        self.table = {self.ip: 0}
+        self.routes = {self.ip: self.table}
 
     def add_address(self, ip, weight):
         self.table[ip] = weight
@@ -31,9 +31,8 @@ class Router:
 
     def update_table(self):
         dist, prev = self.get_routes()
-        dist = {key: dist[key] for key in dist if dist[key] != float("inf")}
-        # talvez filtrar ele mesmo e quem ele nao sabia até entao
-        self.table.update(dist)
+        dist = {key: dist[key] for key in dist if dist[key] != float("inf") and key in self.table}
+        self.routes[self.ip] = dist
 
     def get_routes(self):
         vertices = set()
@@ -47,25 +46,17 @@ class Router:
         prev = {vertex: 0 for vertex in vertices}
         dist[self.ip] = 0
 
-        while vertices:
-            min_ = float("inf")
-            for key in dist:
-                if dist[key] <= min_ and key in  vertices:
-                    min_ = dist[key]
-                    u = key
-            vertices = [vertex for vertex in vertices if vertex != u]
-            if u in self.routes:
-                for neighbor in self.routes[u]:
-                    value = dist[u] + int(self.routes[u][neighbor])
-                    if value < dist[neighbor]:
-                        dist[neighbor] = value
-                        prev[neighbor] = u
+        for _ in range(len(vertices) - 1):
+            for u in self.routes:
+                for v in self.routes[u]:
+                    w = int(self.routes[u][v])
+                    if dist[u] + w < dist[v]:
+                        dist[v] = dist[u] + w
+                        prev[v] = u
 
         return dist, prev
 
     def trace(self, message):
-        dist, prev = self.get_routes()
-        print(prev)
         message["hops"].append(self.ip)
         if self.ip == message["destination"]:
             new_message = {
@@ -79,17 +70,42 @@ class Router:
             client.close()
             return
 
-        vertex = message["destination"]
-        while True:
-            prev_vertex = prev[vertex]
-            if self.ip == prev_vertex:
-                break
-            vertex = prev_vertex
+        min_ = float("inf")
+        for u in self.routes:
+            for v in self.routes[u]:
+                if v == message["destination"] and int(self.routes[u][v] + self.table[u]) <= min_:
+                    print(v, u, message["destination"] )
+                    vertex = u
 
         client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         client.sendto(json.dumps(message).encode(), (vertex, self.port))
         client.close()
 
-# A -> B 10
-# A -> C 1
-# C -> B 1
+
+        # vertices = set()
+        # for vertex in self.routes:
+        #     vertices.add(vertex)
+        #     for neighbor in self.routes[vertex]:
+        #         vertices.add(neighbor)
+
+        # vertices = list(vertices)
+        # dist = {vertex: float("inf") for vertex in vertices}
+        # prev = {vertex: 0 for vertex in vertices}
+        # dist[self.ip] = 0
+
+        # while vertices:
+        #     min_ = float("inf")
+        #     for key in dist:
+        #         if dist[key] <= min_ and key in vertices:
+        #             min_ = dist[key]
+        #             u = key
+        #     vertices = [vertex for vertex in vertices if vertex != u]
+        #     if u in self.routes:
+        #         print(u, self.routes)
+        #         for neighbor in self.routes[u]:
+        #             value = dist[u] + int(self.routes[u][neighbor])
+        #             if value < dist[neighbor]:
+        #                 dist[neighbor] = value
+        #                 prev[neighbor] = u
+
+        # return dist, prev
